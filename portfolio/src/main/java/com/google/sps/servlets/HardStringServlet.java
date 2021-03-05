43d4b8ac5ddfe0bf85abc254b39dbe39b3a65e31
Kevin <kevin.linera@upr.edu>
2021-03-05 18:17:51 +0000

    Create random message using JSON

--- portfolio/src/main/java/com/google/sps/servlets/HardStringServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HardStringServlet.java
@@ -1,5 +1,6 @@
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.Document;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
@@ -15,9 +17,20 @@ public class HardStringServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Random randomNum = new Random();
-    int index = randomNum.nextInt(5);
-    String[] array = {"Hola, yo soy", "Hi, I am", "Aloha, wau", "Salut je suis", "Ciao io sono"};
-    response.getWriter().println(array[index]);
+    ArrayList<String> array = new ArrayList<>();
+    array.add("Hola, yo soy");
+    array.add("Hi, I am");
+    array.add("Aloha, wau");
+    array.add("Salut je suis");
+    array.add("Ciao io sono");
+    String json = convertToJsonUsingGson(array);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
+    // A method that creates a Json file
+    private String convertToJsonUsingGson(ArrayList<String> array) {
+        Gson gson = new Gson();
+        String json = gson.toJson(array);
+        return json;
+    }
 }
--- portfolio/src/main/webapp/script-1.js
+++ portfolio/src/main/webapp/script-1.js
@@ -1,8 +1,12 @@
 async function displayString() {
     const response = await fetch('/hardString');
-    const responseString = await response.text();
-
+    const responseJson = await response.json();
+    // This gets a random index, from the function below
+    let index = randomIndex();
     const greetingContainer = document.getElementById('message-server');
-    greetingContainer.innerText = responseString;
+    greetingContainer.innerText = responseJson[index];
+}
 
+function randomIndex(){
+    return Math.floor(Math.random() * 5);
 }
